643f4e2a1b2512f90a6f9294876471eccaa0251b
NguyenHoangWD229 <hoangWD229+github@gmail.com>
2025-03-01 15:17:27 +0700

    Profile

--- app/Http/Controllers/client/ProfileController.php
+++ app/Http/Controllers/client/ProfileController.php
@@ -3,12 +3,28 @@
 namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Color;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
     public function index()
     {
-        return view('client.pages.profile.index');
+
+        $categories = Category::all();
+        $colors = Color::all();
+
+        return view('client.pages.profile.index', compact('categories', 'colors'));
+    }
+
+    public function show()
+    {
+        $user = Auth::user();
+        $categories = Category::all();
+        $colors = Color::all();
+
+        return view('client.pages.profile.index', compact('user', 'categories', 'colors'));
     }
 }
