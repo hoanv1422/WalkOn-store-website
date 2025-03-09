9f7885a2f1d3e7e18f62c7181827aa323be8b265
mynhle <130372152+mynhle@users.noreply.github.com>
2025-03-09 07:59:56 +0700

    Đẩy lại wishlist + sho

--- app/Http/Controllers/client/WishlistController.php
+++ app/Http/Controllers/client/WishlistController.php
@@ -18,15 +18,12 @@ class WishlistController extends Controller
     
     public function destroy($id)
 {
-    // Tìm sản phẩm trong wishlist của user đăng nhập
     $wishlistItem = Wishlist::where('user_id', Auth::id())->where('id', $id)->first();
 
-    // Kiểm tra nếu sản phẩm không tồn tại hoặc không thuộc về user
     if (!$wishlistItem) {
         return redirect()->route('wishlist.index')->with('error', 'Sản phẩm không tồn tại hoặc không thuộc về bạn.');
     }
 
-    // Xóa sản phẩm
     $wishlistItem->delete();
 
     return redirect()->route('wishlist.index')->with('success', 'Xóa sản phẩm khỏi Wishlist thành công!');
@@ -42,7 +39,7 @@ class WishlistController extends Controller
         $wishlistItem = Wishlist::where('user_id', Auth::id())->where('product_id', $request->product_id)->first();
     
         if ($wishlistItem) {
-            return response()->json(['redirect' => route('wishlist.index')]); // Nếu đã có, chuyển hướng đến wishlist
+            return response()->json(['redirect' => route('wishlist.index')]);
         } else {
             Wishlist::create([
                 'user_id' => Auth::id(),
--- resources/views/client/pages/detail/index.blade.php
+++ resources/views/client/pages/detail/index.blade.php
@@ -2,11 +2,26 @@
 
 @section('title', 'Chi Tiết Sản Phẩm')
 
+@section('style')
+    
+@endsection
+
 @section('content')
     @include('client.components.breadcrumb')
     @include('client.pages.detail.product')
     @include('client.pages.detail.product-tab')
     @include('client.pages.detail.upsell-product')
     @include('client.pages.detail.related-product')
+@endsection
+
+
+@section('script')
+<script>
+
+
+
+
+
+</script>    
 @endsection
 
